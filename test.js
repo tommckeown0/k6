9808e8a8705e5fd60daeaabc8d64b1cf92fc0ee2
Tom Mckeown <112870842+tommckeown0@users.noreply.github.com>
2023-12-13 10:06:55 +0000

    Update test file

--- test.js
+++ test.js
@@ -1,40 +1,54 @@
-import { browser } from 'k6/experimental/browser';
-import { check } from 'k6';
+import { browser } from "k6/experimental/browser";
+import { check, sleep } from "k6";
+import http from "k6/http";
 
 export const options = {
   scenarios: {
     ui: {
-      executor: 'shared-iterations',
+      executor: "shared-iterations",
+      exec: "browserFunction",
       options: {
         browser: {
-          type: 'chromium',
+          type: "chromium",
         },
       },
     },
+    http: {
+      executor: "constant-vus",
+      exec: "httpFunction",
+      vus: 10,
+      duration: "5s",
+    },
   },
   thresholds: {
-    checks: ['rate==1.0'],
+    checks: ["rate==1.0"],
   },
 };
 
-export default async function () {
+export async function browserFunction() {
   const context = browser.newContext();
   const page = context.newPage();
 
   try {
-    await page.goto('https://test.k6.io/my_messages.php');
+    await page.goto("https://test.k6.io/my_messages.php");
 
-    page.locator('input[name="login"]').type('admin');
-    page.locator('input[name="password"]').type('123');
+    page.locator('input[name="login"]').type("admin");
+    page.locator('input[name="password"]').type("123");
 
     const submitButton = page.locator('input[type="submit"]');
 
     await Promise.all([page.waitForNavigation(), submitButton.click()]);
 
     check(page, {
-      header: (p) => p.locator('h2').textContent() == 'Welcome, admin!',
+      header: (p) => p.locator("h2").textContent() == "Welcome, admin!",
     });
   } finally {
     page.close();
   }
+}
+
+export async function httpFunction() {
+  const res = http.get("https://httpbin.test.k6.io/");
+  check(res, { "status was 200": (r) => r.status == 200 });
+  sleep(1);
 }
